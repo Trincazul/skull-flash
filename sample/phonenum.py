b39772869a5b18728d68427da2544b16729c2e17
Endriw Villa <Endriw_villa@icloud.com>
2023-01-25 13:36:51 -0300

    adicionado melhora de identificação de numero

--- sample/phonenum.py
+++ sample/phonenum.py
@@ -3,5 +3,14 @@ from phonenumbers import geocoder
 
 def phonenum():
     phone = input("Digite o telefone no formato ex: +551140019999: ")
-    phone_num = phonenumbers.parse(phone)
-    print(geocoder.description_for_number(phone_num, 'pt'))
+    try:
+        phone_num = phonenumbers.parse(phone)
+        if phonenumbers.is_valid_number(phone_num):
+            print(geocoder.description_for_number(phone_num, 'pt'))
+            print(f"Tipo de número:{phonenumbers.number_type(phone_num)}")
+            print(f"País: {phonenumbers.region_code_for_number(phone_num)}")
+            print(f"Formato nacional: {phonenumbers.format_number(phone_num, phonenumbers.PhoneNumberFormat.NATIONAL)}")
+        else:
+            print("Número inválido.")
+    except phonenumbers.phonenumberutil.NumberParseException as e:
+        print(e)
