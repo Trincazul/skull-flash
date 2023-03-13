ade016dc07a168c3b4f4f829549a4fb6ea55b017
Endriw Villa <Endriw_villa@icloud.com>
2023-03-13 20:43:28 +0000

    improving the architecture of ping functions

--- sample/mapping.py
+++ sample/mapping.py
@@ -1,27 +1,31 @@
 # import nmap
 import os
 
-def main():
+def mapping():
+    pass
+
+def pingmenu():
     print('''Selecione o sistema operacional
                 1) - Mac ou Linux
                 2) - Windows''')
     sisop = int(input())
     ping_host = input("Digite o IP ou Host a ser verificado: ")
     if sisop == 1:
-        macping(ping_host)
+        print("#" * 60)
+        os.system(f'ping -c 6 {ping_host}')
     elif sisop == 2:
-        winping(ping_host)
+        print("#" * 60)
+        os.system(f'ping -n 6 {ping_host}')
     else:
         print('Opção informada incorreta !')
-        return menuindex()
 
-def mapping():
-    pass
-
-def macping(ping_host):
-    print("#" * 60)
-    os.system(f'ping -c 6 {ping_host}')
-
-def winping(ping_host):
-    print("#" * 60)
-    os.system(f'ping -n 6 {ping_host}') #sistema Win
+def main():
+    option = int(input('''Selecione -> 1 para mapeamento de rede:
+Selecione -> 2 para fazer um ping simples no IP:  '''))
+    if option == 1:
+        print('Numero 1 foi selecionado')
+    elif option == 2:
+        print('Numero 2 foi selecionado')
+        pingmenu()
+    else:
+        print('Opção errada')
